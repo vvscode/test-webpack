52bbc4554f509db3d420292d07a9e6d9086faa4f
Vasil Vanchuk <v.vanchuk@tut.by>
2020-11-30 20:57:43 +0200

    Downgrade webpack and setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,22 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js'
   },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },  
   plugins: [new HtmlWebpackPlugin()]
 };
