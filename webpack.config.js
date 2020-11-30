ba25cc59fc760221e69cb04e5a99e8aace8772d6
Vasil Vanchuk <v.vanchuk@tut.by>
2020-11-30 20:33:24 +0200

    Setup webpack html plugin

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
